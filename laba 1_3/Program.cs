07e6e9a187d47c46a567f6e2775de9edab0c95f3
Николай <@DESKTOP-8VK6QD8>
2022-03-16 22:52:19 +0300

    Добавьте файлы проекта.

---
+++ laba 1_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace qwerty 
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Restaurant { get; set; }
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+            {
+                Person Paul = new Person() 
+                { 
+                    Name = "Paul Allen", Age = 26, Restaurant = "Dorsia" 
+                };
+                await JsonSerializer.SerializeAsync<Person>(fs, Paul);
+                Console.WriteLine("Данные сохранены");
+            }
+
+            using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+            {
+                Person restoredPerson = await JsonSerializer.DeserializeAsync<Person>(fs);
+                Console.WriteLine($"Name: {restoredPerson.Name}  Age: {restoredPerson.Age}  Restaurant: {restoredPerson.Restaurant}");
+            }
+            File.Delete("user.json");
+            Console.WriteLine("Файл удалён");
+        }
+    }
+}
